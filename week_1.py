20306bb52c2379090c93366cf132058a78356891
Asante-ntiro <asante.ntiro@gmail.com>
2022-02-14 20:13:08 +0300

    Added comments

--- week_1.py
+++ week_1.py
@@ -1,11 +1,13 @@
 import math
 
+#does not consider wrong input e.g. string
 n = int(input("Enter number to see if it is in the Fibonacci Sequence "))
 
+#helper function
 def isPerfectSquare(x):
     s = math.sqrt(x)
     return s*s == x
-
+#a number is in the fibonacci sequence iff the below values are perfect squares
 def isFibonacci(n):
     return isPerfectSquare(5*n*n + 4) or isPerfectSquare(5*n*n - 4)
 
